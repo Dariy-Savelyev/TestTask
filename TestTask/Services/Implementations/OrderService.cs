8f7a4b763a2e6175618c26834d69627813f1a899
Dariy Savelyev <dariy.savelyev@gmail.com>
2024-11-14 19:01:02 +0300

    Initial Commit

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService(ApplicationDbContext context) : IOrderService
+{
+    public async Task<Order> GetOrder()
+    {
+        var order = await context.Orders
+            .Where(x => x.Quantity > 1)
+            .OrderByDescending(x => x.CreatedAt)
+            .FirstOrDefaultAsync();
+
+        return order!;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        var orders = await context.Orders
+            .Where(x => Equals(x.User.Status, UserStatus.Active))
+            .OrderBy(x => x.CreatedAt)
+            .ToListAsync();
+        
+        return orders;
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService(ApplicationDbContext context) : IUserService
+{
+    public async Task<User> GetUser()
+    {
+        var user = await context.Users
+            .Select(x => new
+            {
+                User = x,
+                TotalProductValue = x.Orders
+                    .Where(y => y.Status == OrderStatus.Delivered && y.CreatedAt.Year == 2003)
+                    .Sum(y => y.Price * y.Quantity)
+            })
+            .OrderByDescending(x => x.TotalProductValue)
+            .Select(x => x.User)
+            .FirstOrDefaultAsync();
+
+        return user!;
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        var users = await context.Users
+            .Where(x => x.Orders.Any(y => y.Status == OrderStatus.Paid && y.CreatedAt.Year == 2010))
+            .ToListAsync();
+
+        return users;
+    }
+}
